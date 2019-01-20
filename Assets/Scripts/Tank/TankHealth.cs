f713305a6d71e968c4ba55cdb26adc2921641df3
Agresina <agresina@gmail.com>
2019-01-20 21:41:43 +0100

    Health system

--- Assets/Scripts/Tank/TankHealth.cs
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -10,7 +10,6 @@ public class TankHealth : MonoBehaviour
     public Color m_ZeroHealthColor = Color.red;    
     public GameObject m_ExplosionPrefab;
     
-    /*
     private AudioSource m_ExplosionAudio;          
     private ParticleSystem m_ExplosionParticles;   
     private float m_CurrentHealth;  
@@ -32,23 +31,54 @@ public class TankHealth : MonoBehaviour
         m_Dead = false;
 
         SetHealthUI();
-    }
-    */
+    }    
 
+    /**
+     * Public so to be used on firing system
+     **/
     public void TakeDamage(float amount)
     {
         // Adjust the tank's current health, update the UI based on the new health and check whether or not the tank is dead.
+        // Reduce health
+        m_CurrentHealth -= amount;
+
+        // Update Health UI (Slider)
+        SetHealthUI();
+
+        // Dying
+        if(m_CurrentHealth <= 0f && !m_Dead)
+        {
+            // Death event
+            OnDeath();
+        }
     }
 
 
     private void SetHealthUI()
     {
         // Adjust the value and colour of the slider.
+        
+        // Update slider value with health
+        m_Slider.value = m_CurrentHealth;
+
+        // Calculate color
+        m_FillImage.color = Color.Lerp(m_FullHealthColor, m_ZeroHealthColor, m_CurrentHealth/m_StartingHealth);
     }
 
 
     private void OnDeath()
     {
         // Play the effects for the death of the tank and deactivate it.
+        m_Dead = true;
+
+        // Move particles to the tank position
+        m_ExplosionParticles.transform.position = this.transform.position;
+        m_ExplosionParticles.gameObject.SetActive(true);
+
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+
+        // Deactivate the tank
+        this.gameObject.SetActive(false);
     }
 }
